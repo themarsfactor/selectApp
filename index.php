255685e25884c7eb32e2a59efe7805c82da5e3c5
Yemi <olayemey@gmail.com>
2023-09-01 09:21:48 +0100

    first

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>selectApp</title>
+	<link rel="stylesheet" type="text/css" href="thirdparties/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+
+	<div style="display: none;" id="content1">
+		<p>The Faculty is a cluster or group of members interested in the pursuit of knowledge, research and advancement in a particular area of expertise in the practice of accounting which includes but not limited to the following:
+		</p>
+
+		<ul>
+			<li>Audit, Investigation and Forensic</li>
+			<li>Information Technology and Consulting</li>
+			<li>Corporate Finance Management</li>
+			<li>Corporate Reporting</li>
+			<li>Insolvency and Corporate Re-engineering</li>
+			<li>Public Finance Management</li>
+			<li>Taxation and Fiscal Policy.</li>
+		</ul>
+
+		<p>As centres of excellence, the seven Faculties are to provide technical support to members as well as offer them opportunities to specialize in any area(s) of their choice. Faculties are therefore conceived as service and capacity building units of the Institute designed to support members as they carry out their respective value creating activities. Faculties register, train, and issue certificates to their members as evidence of proficiency and competence in their area of specialization.</p>
+		
+	</div>
+
+
+
+	<div style="display: none;" id="content2">
+		<p> Membership of the Faculty is open to all members of the Institute. Faculty registration can be done <strong><i>MANUALLY or ON-LINE.</i></strong>  For 
+			<strong><i>Manual registration:</i></strong> </p>
+
+		<ol>
+			<li>Obtain Faculty form from the Institute’s office or download copy from ICAN WEBSITE</li>
+			<li>Complete the form with necessary details.</li>
+			<li>Attach evidence of payment to the form</li>
+			<li>Submit at ICAN office.</li>
+		</ol>
+
+		<p>For <strong><i>On-line registration:</i></strong> </p>
+
+		<ol>
+			<li>Obtain Pin Voucher from the designated branches of Zenith Bank Plc.</li>
+			<li>Log on to http: www.ican-ngr.org/faculty and select the Faculty of your choice.</li>
+			<li>Complete the registration form</li>
+			<li>Print out the Faculty registration confirmation form and keep for your record.</li>
+		</ol>
+
+		<p>Please note that the on-line registration is not complete until you have successfully printed the Faculty registration confirmation form. <strong>Faculty Fees: </strong> For individual members: Registration: N2,500 per Faculty Annual subscription: N2,500 per Faculty For corporate members: Registration: N5,000 per Faculty Annual subscription: N5,000 per Faculty
+</p>
+
+		
+	</div>
+
+
+
+	<div style="display: none;" id="content3">
+		<p>A member is allowed to belong to a minimum of 1 (one) Faculty and maximum of 3 (three) Faculties.</p>
+		
+	</div>
+
+
+
+	<div style="display: none;" id="content4">
+		The dissemination of information is via email, SMS, and social media handles(Facebook, Instagram, Twitter, LinkedIn) of the Institute text message and newspaper advertisements.
+		
+	</div>
+
+
+	<div style="display: none;" id="content5">
+		<p>All stakeholders benefit from the creation of specialized Faculties.</p>
+
+		<ul>
+			<li>For members, it is an opportunity to specialize in area(s) of core competence without having to belong to another professional body.</li>
+			<li>The Faculties issue certificate of such competence in line with approved guidelines.</li>
+			<li>They also provide opportunity for capacity building and skill enhancement at minimal cost, and access to regular technical support from the Institute.</li>
+			<li>Also, there is sharing of experience and networking which significantly impacts the quality of professional work.</li>
+		</ul>
+		
+	</div>
+
+
+	<div style="display: none;" id="content6">
+
+		<p>
+			<ul>
+				<li>There are seven Faculties, namely:</li>
+				<li>Audit, Investigations and Forensic Accounting Faculty</li>
+				<li>Information Technology and Consulting Faculty</li>
+				<li>Corporate Finance Management Faculty</li>
+				<li>Corpoate Reporting Faculty</li>
+				<li>Insolvency and Corporate Re-Engineering Faculty</li>
+				<li>Public Finance Management Faculty</li>
+				<li>Taxation and Fiscal Policy Faculty.</li>
+			</ul>
+		</p>
+		
+	</div>
+
+
+
+	<div style="display: none;" id="content7">
+		<p>All Faculties' programmes are open to all ICAN members whether or not one is a registered member of that Faculty. However, non-members of that Faculty may be required to pay a token fee to participate.</p>
+		
+	</div>
+
+
+	<div style="display :none;" id="content8">
+		<p> Going by the decision of the Council of the Institute, with effect from 2009, members are obliged to belonging to at least one and a maximum of 3 Faculties.</p>
+		
+	</div>
+
+
+	<div style="display : none;" id="content9">
+		<p>After paying the initial registration fee, subsequent fee to be paid is annual subscription falling due on 1st January of each year.</p>
+		
+	</div>
+
+
+	<div style="display : none;" id="content10">
+		<p>Yes. Non-members have been benefitting from the Faculties' programmes by having access to the resources both printed and on-line. However, the certification / proficiency training are currently limited to members only.</p>
+		
+	</div>
+
+
+
+
+	<div style="display: none;" id="content11">
+		<p> If after registration for a particular Faculty and one desires a different one, a formal letter is written to the Institute requesting for a change from the initial Faculty and stating which Faculty one now wants to belong.</p>
+	</div>
+
+
+
+	<div style="display: none;" id="content12">
+		<p> The difficulty in accessing online registration may be attributed to the traffic at the site at a particular time.</p>
+	</div>
+
+
+
+	<div style="display: none;" id="content13">
+		<p>Faculty registration form is not attached to journals, etc because the form is already on ICAN WEBSITE.</p>
+	</div>
+
+
+
+	<div style="display: none;" id="content14">
+		<p>All the programmes of the Faculties attract credit hours, which depends on the depth of coverage.</p>
+	</div>
+
+
+
+	<div style="display: none;" id="content15">
+		<p>The objective of the Faculties is to build professional capacity for service delivery and extend the horizon of members. Therefore, members are in no way restricted from benefiting from knowledge available in any of the Faculties.</p>
+	</div>
+
+	<div class="container bg-light mt-5">
+		<!-- Masthead-->
+        <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+
+                        <div class="text-center text-white">
+                            <!-- Page heading-->
+                            <h1 class="mb-4">Select App</h1>
+                            
+                             <div class="col-12">
+                                <div class="col-xl-8 m-auto">
+                                    
+                                
+                            <form class="form" method="POST">
+                            
+                        <div class="form-group mt-3">
+                            <select class="form-control text-center" id="change">
+                            <option value="0" selected="selected">SELECT OPTION</option>
+                                    <optgroup>
+                                        
+                                        <option value="1">What is the Faculty all about?</option>
+                                        <option value="2">How do I go about registering for the Faculty?</option>
+                                        <option value="3">How many Faculties is a member allowed to belong?</option>
+                                        <option value="4">How is information about the Faculty disseminated to members?</option>
+                                        <option value="5">What are the benefits of belonging to a Faculty?</option>
+                                        <option value="6">How many Faculties are there?</option>
+                                        <option value="7">Can I attend seminars organized by a Faculty without belonging to that Faculty?</option>
+                                        <option value="8">Is it compulsory to belong to a Faculty?
+                                        </option>
+                                        <option value="9">After registration into a Faculty, are there more fees to be paid subsequently?</option>
+                                        <option value="10">Can non-members of the Institute benefit from the resources of the Faculty?</option>
+                                        <option value="11">Can I change my Faculty? </option>
+                                        <option value="12">Why is online registration difficult to access?</option>
+                                        <option value="13">Why is Faculty registration form not attached to journals, newsletters and Demand Note when dispatching such materials?</option>
+                                        <option value="14">What number of credit hours is assigned to each Faculty?</option>
+                                        <option value="15">Can I register for a Faculty outside my area of business?</option>
+                                    </optgroup> 
+                            </select>
+                        </div>
+
+                        <div class="col-md-12 mt-3">
+                           
+                            <div class="col-md-4 m-auto">
+                           <button type="button" class="btn btn-lg btn-primary" id="ibtn"> Search </button>
+                           
+                            </div>
+                        </div>
+
+                           </form> 
+                       </div>
+
+                             </div> 
+
+
+
+                                
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+			
+			
+		</div>
+		
+	</div>
+
+				<div class=" row">
+					<div class="col-12">
+					<div class="col-xl-8">
+                    <h5 class='mt-3' id="search-results-heading"></h5>
+                    <div id="search-results"></div>
+                    </div>
+                    </div>
+                    </div> 
+
+
+
+
+	<script type="text/javascript">
+		
+
+		let btn = document.querySelector("#ibtn");
+		btn.addEventListener("click", function(){
+
+
+			let mysec = document.querySelector("#change");
+
+			if (mysec.value == 0 ) {
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = "Nothing is Selected!";
+				
+			}else if (mysec.value == 1) {
+				let cont1 = document.querySelector("#content1").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont1;
+			}
+			else if (mysec.value == 2) {
+
+				let cont2 = document.querySelector("#content2").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont2;
+
+
+			}
+
+
+			else if (mysec.value == 3) {
+
+				let cont3 = document.querySelector("#content3").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont3;
+
+
+			}
+
+
+
+			else if (mysec.value == 4) {
+
+				let cont4 = document.querySelector("#content4").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont4;
+
+
+			}
+
+
+
+			else if (mysec.value == 5) {
+
+				let cont5 = document.querySelector("#content5").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont5;
+
+
+			}
+
+
+
+			else if (mysec.value == 6) {
+
+				let cont6 = document.querySelector("#content6").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont6;
+
+
+			}
+
+
+			else if (mysec.value == 7) {
+
+				let cont7 = document.querySelector("#content7").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont7;
+
+
+			}
+
+
+			else if (mysec.value == 8) {
+
+				let cont8 = document.querySelector("#content8").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont8;
+
+
+			}
+
+
+
+			else if (mysec.value == 9) {
+
+				let cont9 = document.querySelector("#content9").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont9;
+
+
+			}
+
+
+
+			else if (mysec.value == 10) {
+
+				let cont10 = document.querySelector("#content10").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont10;
+
+
+			}
+
+
+
+
+			else if (mysec.value == 11) {
+
+				let cont11 = document.querySelector("#content11").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont11;
+
+
+			}
+
+
+
+			else if (mysec.value == 12) {
+
+				let cont12 = document.querySelector("#content12").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont12;
+
+
+			}
+
+
+
+			else if (mysec.value == 13) {
+
+				let cont13 = document.querySelector("#content13").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont13;
+
+
+			}
+
+
+			else if (mysec.value == 14) {
+
+				let cont14 = document.querySelector("#content14").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont14;
+
+
+			}
+
+
+
+
+			else if (mysec.value == 15) {
+
+				let cont15 = document.querySelector("#content15").innerHTML;
+				let displayResult = document.querySelector("#search-results");
+				displayResult.innerHTML = cont15;
+
+
+			}
+
+
+
+
+
+
+
+
+
+		})
+		
+
+
+	</script>
+
+
+
+</body>
+</html>
